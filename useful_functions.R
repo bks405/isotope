f866fc03eef46fe62e5d30c1297f4cf35a40fe49
bks405 <62579348+bks405@users.noreply.github.com>
2020-04-10 14:08:47 -1000

    Update useful_functions.R

--- useful_functions.R
+++ useful_functions.R
@@ -37,3 +37,18 @@ dqsat_dT <- function(p,T){
 	dqsat_dT = (L_v*e_sat/R_v/T^2)*eps*p/(p-(1-eps)*e_sat)^2
 	
 }
+
+# find alpha
+a_eq <- function(n_iso, Temp) {
+if (n_iso == 1) {          # % Deuterium
+  a1 = 24884
+  a2 = -76.248
+  a3 = 0.052612
+  }
+if (n_iso == 2) {          # % 180
+  a1 = 1137
+  a2 = -0.4156
+  a3 = -0.0020667
+  }
+}
+a_eq = exp((a1 + Temp * (a2 + Temp * a3))/(Temp*Temp))
