56f44d515a8997dddb869c374c98d028d21b1e37
bks405 <62579348+bks405@users.noreply.github.com>
2020-04-10 14:35:19 -1000

    Update useful_functions.R

--- useful_functions.R
+++ useful_functions.R
@@ -1,41 +1,40 @@
 e_sat <- function(T){
-	
-	T0    = 273.15
-	Lv    = 2.5e6
-	Rv    = 461
-	
-	e_sat = 611.2*exp(Lv/Rv*(1/T0-1/T))
-	
+  
+  T0    = 273.15
+  Lv    = 2.5e6
+  Rv    = 461
+  
+  e_sat = 611.2*exp(Lv/Rv*(1/T0-1/T))
+  
 }
-​
-​
-​
+
+
 q_sat <- function(p,T){
-	
-	T0    = 273.15
-	Lv    = 2.5e6
-	Rv    = 461
-	Rd    = 287
-	eps   = Rd/Rv
-	
-	e_sat = 611.2*exp(Lv/Rv*(1/T0-1/T))
-	q_sat = eps*e_sat/(p-(1-eps)*e_sat)
-​
+  
+  T0    = 273.15
+  Lv    = 2.5e6
+  Rv    = 461
+  Rd    = 287
+  eps   = Rd/Rv
+  
+  e_sat = 611.2*exp(Lv/Rv*(1/T0-1/T))
+  q_sat = eps*e_sat/(p-(1-eps)*e_sat)
+  
 }
-​
-​
+
+
 dqsat_dT <- function(p,T){
-	
-	T0       = 273.15
-	Lv       = 2.5e6
-	Rv       = 461
-	Rd       = 287
-	eps      = Rd/Rv
-	
-	e_sat    = 611.2*exp(Lv/Rv*(1/T0-1/T))
-	
-	dqsat_dT = (L_v*e_sat/R_v/T^2)*eps*p/(p-(1-eps)*e_sat)^2
-	
+  
+  T0       = 273.15
+  Lv       = 2.5e6
+  Rv       = 461
+  Rd       = 287
+  eps      = Rd/Rv
+  
+  e_sat    = 611.2*exp(Lv/Rv*(1/T0-1/T))
+  
+  dqsat_dT = (L_v*e_sat/R_v/T^2)*eps*p/(p-(1-eps)*e_sat)^2
+  
 }
 
 # find alpha
@@ -50,5 +49,5 @@ if (n_iso == 2) {          # % 180
   a2 = -0.4156
   a3 = -0.0020667
   }
+  a_eq = exp((a1 + Temp * (a2 + Temp * a3))/(Temp*Temp))
 }
-a_eq = exp((a1 + Temp * (a2 + Temp * a3))/(Temp*Temp))
